bd73f3a3ad2dfab305ecdbc98a717541cbab64ad
Christian Portoghesi <christian.portoghesi@digi-one.eu>
2026-01-09 14:51:50 +0100

    added login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 
 import Register from "./pages/Register"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
